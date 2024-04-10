4a1ccc1435898c801039623b1f05ca95a4063ad2
Kshitij Gupta <kshitijqwerty@gmail.com>
2024-04-10 14:40:10 +0530

    made changes to tranform

--- misc.py
+++ misc.py
@@ -11,10 +11,11 @@ class CustomTransforms:
     def transformA10():
         return A.Compose(
                     [
-                        A.PadIfNeeded(min_height=36, min_width=36, border_mode=cv2.BORDER_CONSTANT, value=0),
+                        A.PadIfNeeded(min_height=40, min_width=40, border_mode=cv2.BORDER_CONSTANT, value=(0.4914, 0.4822, 0.4465), always_apply=True),
                         A.RandomCrop(width=32, height=32),
                         A.HorizontalFlip(p=0.5),
                         A.CoarseDropout (max_holes = 1, max_height=8, max_width=8, min_holes = 1, min_height=8, min_width=8, fill_value=0.4734, mask_fill_value = None),
+                        A.Normalize(mean = (0.4914, 0.4822, 0.4465), std = (0.2023, 0.1994, 0.2010),p =1.0),
                         ToTensorV2()
                     ]
                 )
@@ -26,3 +27,24 @@ def max_lr_finder(model, train_loader, test_loader, device):
     lr_finder.range_test(train_loader, val_loader=test_loader, end_lr=1, num_iter=100, step_mode="linear")
     lr_finder.plot(log_lr=False)
     lr_finder.reset()
+
+
+class OneCyclePolicy:
+    def __init__(self, max_lr, min_lr, epochs, steps_per_epoch, peak_epoch):
+        self.max_lr = max_lr
+        self.min_lr = min_lr
+        self.epochs = epochs
+        self.steps_per_epoch = steps_per_epoch
+        self.peak_epoch = peak_epoch
+        self.current_epoch = 0
+
+    def get_lr(self):
+        if self.current_epoch < self.peak_epoch:
+            lr = self.min_lr + (self.max_lr - self.min_lr) * self.current_epoch / self.peak_epoch
+        else:
+            lr = self.max_lr - (self.max_lr - self.min_lr) * (self.current_epoch - self.peak_epoch) / (self.epochs - self.peak_epoch)
+        return lr
+
+    def step(self):
+        self.current_epoch += 1
+        return self.get_lr()
