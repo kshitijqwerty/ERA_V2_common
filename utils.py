c8a067f3e376c78265088c908fb0097e7d17bddf
Kshitij Gupta <kshitijqwerty@gmail.com>
2024-04-12 21:46:30 +0530

    added numpy to utils

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image
 import torch
 import matplotlib.pyplot as plt
+import numpy as np
 
 def transform_cifar_A10():
     return A.Compose(
